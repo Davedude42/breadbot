0acac13a2cdde8c1373d80f32f04d0fb33696731
David Haroldsen <davidharoldsen42@gmail.com>
2024-12-21 19:12:35 -0700

    Wrote it

---
+++ README.md
@@ -0,0 +1,41 @@
+# Bread Bot
+
+Bread Bot sends a message twice weekly to our LDS church's youth Group Me, giving the lesson assignments and bread assignment for the next Sunday.
+
+Created by David using Node on Cloudflare, Group Me API, and Sheety. 
+
+## Dev Process
+
+### Local Installation
+
+To edit locally: clone the repo to your device and run `npm install`, assuming Node and npm is installed on your device. Use Wrangler commands to debug and deploy the project.
+
+#### Wrangler Commands
+
+Open local preview: `npx wrangler dev` (Note that for some reason, the API keys are not defined under `env`, so it won't be able to fetch data)
+
+Deploy project and upload to Cloudflare: `npx wrangler deploy`
+
+### Cloudflare Worker
+
+The Cloudflare worker is owned by the Bread Bot account. Use the Cloudflare dashboard to edit settings and view logs. After logging into Cloudflare, you can always find the worker under Workers & Pages in the sidebar. You can edit the code on the dashboard, but it's compiled weirdly before being deployed, so I recommend not.
+
+#### cron Timer
+
+A cron timer, which can be found under `Settings > Trigger Events` on the dashboard, runs `scheduled()` every Tuesday, Wednesday, and Friday at 11pm UTC. Depending on daylight savings, this is either 5pm CST or 6pm CDT.
+
+#### Preview
+
+The preview of the site runs `fetch()`, which responds the debug message without sending a Group Me message. Can be used to debug the upcoming message anytime.
+
+### Group Me API
+
+Group Me API account is owned by David. Messages are sent to Group Me API using the separate API keys for the debug bot and the real bot. If something about the bots needs to be changed, let David know. Custom messages can also be sent by any bot on the API site by request.
+
+### Sheety
+
+Sheety account is the Bread Bot account. 
+
+Bread Bot owns a (Google Sheet)[https://docs.google.com/spreadsheets/d/1J_AXqTPNxjC9bsmy4_pKCLjYsfktHLEnYcrNE96tmCY/edit?gid=0#gid=0] that imports the youth planner data using `IMPORTRANGE` (which is found under cell A2 of the sheet). Sheety reads the Google Sheet and makes it into an API. 
+
+Note: The free version of Sheety only allows 100 rows of data. If the import range is not shifted down within... 2 years, data will be cut off by Sheety.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,202 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
+function replaceNoValue(value) {
+	if(value === '' || value === undefined || value === null || value === false || value === '-') {
+		return '(None Listed)';
+	} else {
+		return value;
+	}
+}
+
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+async function getBreadbotData(sheetyKey) {
+	let res = await fetch('https://api.sheety.co/9b81eca6e097f927c510daac7996c3aa/breadBotData/sheet1', {
+		headers: {
+			Authorization: 'Bearer ' + sheetyKey
+		}
+	});
+	let data = await res.json();
+
+	return data.sheet1;
+}
+
+function getTodaysEntry(data, now) {
+	let nowDate = new Date(now);
+
+	let i = 0;
+	let entryDate;
+	do {
+		entryDate = new Date(data[i].date);
+		i++;
+
+		if(i >= data.length) {
+			return null;
+		}
+	} while(entryDate < nowDate);
+
+	return data[i];
+}
+
+function composeBreadMessage(entry) {
+	let lines = [];
+	let formattedDate = (new Date(entry.date)).toLocaleDateString();
+
+	if(entry.event && entry.event.toUpperCase() != 'FAST SUNDAY' && entry.event.toUpperCase() != 'PRIMARY PROGRAM') {
+		// Special event, likely no sacrament meeting or second hour
+
+		lines.push(`Hi! ${toTitleCase(entry.event)} is this Sunday, ${formattedDate}.`);
+
+		if(!entry.bread) {
+			lines.push(`No bread assignment this week!`);
+		}
+	} else {
+		// Options for second hour
+		if(entry.secondHour.toUpperCase() == 'SS') {
+			lines.push(`Hi! We have Sunday School this Sunday, ${formattedDate}.`);
+		} else if(entry.secondHour.toUpperCase() == 'ORG') {
+			lines.push(`Hi! We have Young Women's/Priesthood this Sunday, ${formattedDate}.`);
+		} else if(entry.secondHour.toUpperCase() == '5TH') {
+			lines.push(`Hi! We have 5th Sunday this week, ${formattedDate}.`);
+		} else if(entry.secondHour.toUpperCase() == 'NONE') {
+			lines.push(`Hi! We have no second hour this week, ${formattedDate}.`);
+		} else {
+			// Silently report error
+			// Second hour data will be missing, but it's better that the message just gets send
+			console.error({ message: 'Unexpected second hour value', entry });
+		}
+
+		if(entry.event.toUpperCase() == 'FAST SUNDAY') {
+			lines.push(`Also, don't forget it's Fast Sunday this week!`);
+		}
+	}
+
+	if(entry.bread) {
+		lines.push(`Bread Assignment: ${replaceNoValue(entry.bread)}`);
+	}
+
+	return lines.join('\n');
+}
+
+function composeLessonMessage(entry) {
+	let lines = [];
+	let formattedDate = (new Date(entry.date)).toLocaleDateString();
+
+	if(
+		!(entry.event && entry.event.toUpperCase() != 'FAST SUNDAY' && entry.event.toUpperCase() != 'PRIMARY PROGRAM') && 
+		(entry.secondHour?.toUpperCase() == 'ORG')
+	) {
+		// Not special event and second hour is young women's/priesthood
+
+		lines.push(`Hi! Here's your reminder for priesthood lessons for ${formattedDate}.`);
+		if(entry.deacon?.toLowerCase()?.includes('combined')) {
+			lines.push('Combined young men\'s lesson this week')
+		} else {
+			lines.push(`Deacons:  ${replaceNoValue(entry.deacons)}`);
+			lines.push(`Teachers: ${replaceNoValue(entry.teachers)}`);
+			lines.push(`Priests:  ${replaceNoValue(entry.priests)}`);
+		}
+
+		return lines.join('\n');
+		
+	} else {
+		return false;
+	}
+}
+
+function composeDebugMessage(entry) {
+	let breadMessage = composeBreadMessage(entry);
+	let lessonMessage = composeLessonMessage(entry);
+
+	return `
+${JSON.stringify(entry)}
+
+${lessonMessage === false ? '[No lesson message this week]' : lessonMessage}
+
+${breadMessage}
+`.trim();
+}
+
+async function sendMessage(key, message) {
+	let res = await fetch('https://api.groupme.com/v3/bots/post',
+		{
+			method: 'POST',
+			body: JSON.stringify({
+				"bot_id"  : key,
+				"text"    : message
+			})
+		}
+	);
+
+	return res;
+}
+
 
 export default {
+	/**
+	 * Runs when worker is previewed.
+	 * Responds debug message.
+	 */
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+
+		let data = await getBreadbotData(env.SHEETY_KEY);
+		let todaysEntry = getTodaysEntry(data, new Date());
+
+    return new Response(composeDebugMessage(todaysEntry));
+  },
+
+	/**
+	 * Run by cron timer every Tuesday, Wednesday, and Friday at 11pm UTC.
+	 */
+	async scheduled(event, env, ctx) {
+		let schedTime = new Date(event.scheduledTime);
+
+		let data = await getBreadbotData(env.SHEETY_KEY);
+		let todaysEntry = getTodaysEntry(data, new Date());
+
+		let dayOfWeek = schedTime.getDay();
+
+		// Tuesday (debug day)
+		if(dayOfWeek == 2) {
+			let message = composeDebugMessage(todaysEntry);
+			if(message !== null) {
+				let res = await sendMessage(env.DEBUG_KEY, message);
+				if(!res.ok) {
+					let response = await res.text();
+					console.error({ code: res.status, response });
+				}
+				console.log({ type: 'debug', message });
+			}
+		}
+		// Wednesday
+		else if(dayOfWeek == 3) {
+			let message = composeLessonMessage(todaysEntry);
+			if(message !== null) {
+				let res = await sendMessage(env.DEBUG_KEY, message);
+				if(!res.ok) {
+					let response = await res.text();
+					console.error({ code: res.status, response });
+				}
+				console.log({ type: 'wednesday', message });
+			}
+		}
+		// Friday
+		else if(dayOfWeek == 5) {
+			let message = composeBreadMessage(todaysEntry);
+			if(message !== null) {
+				let res = await sendMessage(env.DEBUG_KEY, message);
+				if(!res.ok) {
+					let response = await res.text();
+					console.error({ code: res.status, response });
+				}
+				console.log({ type: 'friday', message });
+			}
+		}
+  }
 };
